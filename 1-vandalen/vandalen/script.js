1ea8e8e6c6339bff011b5336faaf133c3d9141b3
eh222ve <eh222ve@student.lnu.se>
2014-11-06 14:50:07 +0100

    Finished object manipulation

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,9 +1,24 @@
 "use strict";
 
+var names;
+var ages;
+var sumAges;
+
+var splitPerson = function(person){
+	ages.push(person.age);
+	sumAges += person.age;
+	names.push(person.name);
+};
+
 var makePerson = function(persArr){
 
+	names = [];
+	ages = [];
+	sumAges = 0;
 
-	// Din kod här...
+	persArr.forEach(splitPerson);
 
-}
+	ages = ages.sort();
 
+	return {names: names.sort(String.localeCompare).join(', '), minAge: ages[0], maxAge: ages[persArr.length-1], averageAge: Math.round(sumAges/persArr.length)};
+};
