b80753a9312b1e52ead80bddc64d7ad1b9448fbe
eh222ve <eh222ve@student.lnu.se>
2014-11-09 15:58:53 +0100

    Reorganized methods

--- 2-labbymezzage/MessageBoard.js
+++ 2-labbymezzage/MessageBoard.js
@@ -1,15 +1,29 @@
 "use strict";
 
 function MessageBoard(containerId, title){
-    title = (title !== null ? title : 'LabbyMezzages');
+    var that = this;
+    this.title = (title !== null ? title : 'LabbyMezzages');
 
     this.rootId = document.getElementById(containerId);
-    var that = this;
+
     this.messages = [];
+
+    this.CreateHTMLLayout();
+
+    this.renderMessages();
+
+    setInterval(function () {
+        that.renderMessages();
+    }, 1000);
+
+}
+
+MessageBoard.prototype.CreateHTMLLayout = function(){
+    var that = this;
 
     var labbyHeader = document.createElement("header");
     var labbyHeaderText = document.createElement("h1");
-    labbyHeaderText.innerHTML = title;
+    labbyHeaderText.innerHTML = this.title;
     labbyHeader.appendChild(labbyHeaderText);
     this.rootId.appendChild(labbyHeader);
 
@@ -41,67 +55,49 @@ function MessageBoard(containerId, title){
 
     this.rootId.appendChild(labbySubmit);
 
-
-    this.addMessage = function(){
-        var textArea = that.rootId.getElementsByClassName("labbyMezzageContent")[0];
-
-        that.messages.push(new Message(textArea.value, new Date()));
-
-        textArea.value = '';
-
-        that.renderMessages();
-        that.scrollToBottom();
-    };
-
     labbySubmit.onclick = function(){
         that.addMessage();
         that.rootId.getElementsByClassName("labbyMezzageContent")[0].focus();
     };
-
-    this.renderMessage = function(message, count){
-        var messageMain = document.createElement("section");
-
-        var messageRemove = document.createElement("img");
-        messageRemove.alt="Close Message";
-        messageRemove.src ="images/Close.png";
-        messageRemove.onclick = function(){
-            if (window.confirm("Do you want to remove the message?")) {
-                that.messages.splice(count, 1);
-                that.renderMessages();
-                that.scrollToBottom();
-            }
-        };
-        messageMain.appendChild(messageRemove);
-
-        var messageTime = document.createElement("img");
-        messageTime.alt="View time information";
-        messageTime.src ="images/Clock.png";
-        messageTime.onclick = function(){
-            alert("Message created on: (" + message.getDate() + ")");
-        };
-        messageMain.appendChild(messageTime);
-
-        var messageText = document.createElement("p");
-        messageText.innerHTML = message.getHTMLText();
-        messageMain.appendChild(messageText);
-
-        var messageDate = document.createElement("date");
-        messageDate.innerHTML = message.getDateText();
-        messageMain.appendChild(messageDate);
-
-        return messageMain;
-    };
-    that.renderMessages();
-
-    setInterval(function () {
-        that.renderMessages();
-    }, 1000);
-    //setInterval(that.renderMessages(), 5000);
-
-}
+};
 
 MessageBoard.prototype.numberOfMessages = function(){
         return this.messages.length + " messages";
+};
+
+MessageBoard.prototype.renderMessage = function(message, count){
+    var that = this;
+    var messageMain = document.createElement("section");
+
+    var messageRemove = document.createElement("img");
+    messageRemove.alt="Close Message";
+    messageRemove.src ="images/Close.png";
+    messageRemove.onclick = function(){
+        if (window.confirm("Do you want to remove the message?")) {
+            that.messages.splice(count, 1);
+            that.renderMessages();
+            that.scrollToBottom();
+        }
+    };
+    messageMain.appendChild(messageRemove);
+
+    var messageTime = document.createElement("img");
+    messageTime.alt="View time information";
+    messageTime.src ="images/Clock.png";
+    messageTime.onclick = function(){
+        alert("Message created on: (" + message.getDate() + ")");
+    };
+    messageMain.appendChild(messageTime);
+
+    var messageText = document.createElement("p");
+    messageText.innerHTML = message.getHTMLText();
+    messageMain.appendChild(messageText);
+
+    var messageDate = document.createElement("date");
+    messageDate.innerHTML = message.getDateText();
+    messageMain.appendChild(messageDate);
+
+    return messageMain;
 };
 
 MessageBoard.prototype.renderMessages = function(){
@@ -122,4 +118,17 @@ MessageBoard.prototype.renderMessages = function(){
 MessageBoard.prototype.scrollToBottom = function(){
     var messageArea = this.rootId.getElementsByClassName("labbyMezzageArea")[0];
     messageArea.scrollTop = messageArea.scrollHeight;
+};
+
+MessageBoard.prototype.addMessage = function(){
+    var textArea = this.rootId.getElementsByClassName("labbyMezzageContent")[0];
+    console.log(textArea.value);
+
+    if(textArea.value != "") {
+        this.messages.push(new Message(textArea.value, new Date()));
+    }
+    textArea.value = '';
+
+    this.renderMessages();
+    this.scrollToBottom();
 };
