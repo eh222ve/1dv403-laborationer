02e5994216114113c1afca31f4247163e06c3fda
eh222ve <eh222ve@student.lnu.se>
2014-11-13 12:07:57 +0100

    Removed forEach loop, now using map

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -2,28 +2,29 @@
 
 var makePerson = function(persArr){
 
-	var names = [];
-	var ages = [];
-	var sumAges = 0;
-
-	persArr.forEach(function(person){
+	var names = persArr.map(function(person) {
 		try {
-			if (typeof person.age != 'number' || (person.age % 1) !== 0 ) {
-				throw new TypeError("Age of " + person.name + " (\"" + person.age + "\") is not valid");
-			}
-			ages.push(person.age);
-			sumAges += person.age;
-
 			if (typeof person.name != 'string') {
 				throw new TypeError("Name: \"" + person.name + "\" is not a valid string");
 			}
-			names.push(person.name);
+			return person.name;
 		}catch(TypeError){
-			console.log(TypeError.message);
+			console.warn(TypeError.message);
 		}
-	});
+	}).sort(String.localeCompare).join(', ');
 
-	ages = ages.sort();
+	var ages = persArr.map(function(person) {
+		try {
+			if (typeof person.age != 'number' || (person.age % 1) !== 0) {
+				throw new TypeError("Age of " + person.name + " (\"" + person.age + "\") is not valid");
+			}
+			return person.age;
+		}catch(TypeError){
+		console.warn(TypeError.message);
+		}
+	}).sort();
 
-	return {names: names.sort(String.localeCompare).join(', '), minAge: ages[0], maxAge: ages[persArr.length-1], averageAge: Math.round(sumAges/persArr.length)};
+	var averageAge = Math.round(ages.reduce(function(a, b) { return a + b; })/persArr.length);
+
+	return {names: names, minAge: ages[0], maxAge: ages[ages.length-1], averageAge: averageAge};
 };
