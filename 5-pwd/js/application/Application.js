75f5bcbca46923b840ca0cc010ddbe17d9d731f0
eh222ve <eh222ve@student.lnu.se>
2014-11-28 18:19:40 +0100

    Testing TODO

--- 5-pwd/js/application/Application.js
+++ 5-pwd/js/application/Application.js
@@ -1,11 +1,16 @@
-function Application(type, xPos, yPos){
+"use strict";
+function Application(self, type, xPos, yPos, resize){
+    this.resizable = resize;
+    this.desktop = self;
     this.type = type;
     this.imageFolder = "js/application/images/";
     this.xPos = xPos;
     this.yPos = yPos;
     this.dragY = 0;
     this.dragX = 0;
+    this.app;
     this.createHTML();
+    this.resize = false;
     this.dragging = false;
 }
 Application.prototype.focusWindow = function(e){
@@ -28,24 +33,34 @@ Application.prototype.createHTML = function(){
         header.onmousedown = function(e){
             self.dragY = (e.pageY - divApp.offsetTop);
             self.dragX = (e.pageX - divApp.offsetLeft);
+            header.style.cursor = "move";
             self.dragging = true;
         };
-        header.onmouseup = function() {
+        var disableDragging = function(e){
+            if((e.clientY - self.dragY + 200) > window.innerHeight) {
+                divApp.style.top = (window.innerHeight - 200) + "px";
+            }
+            if((e.clientX - self.dragX + 200) > window.innerWidth) {
+
+                divApp.style.left = (window.innerWidth - 200) + "px";
+            }
             self.dragging = false;
+            header.style.removeProperty("cursor");
         };
-        header.addEventListener("mouseleave", function() {
-            self.dragging = false;
-        });
+        header.onmouseup = disableDragging;
+
+        header.addEventListener("mouseleave", disableDragging);
         header.addEventListener("mousemove", function(e){
             if (self.dragging) {
                 divApp.style.top = (e.clientY - self.dragY)  + "px";
-                divApp.style.left = (e.clientX - self.dragX)  + "px";
+                divApp.style.left = (e.clientX - self.dragX) + "px";
             }
         });
 
         var appIcon = document.createElement("img");
             appIcon.src = this.imageFolder + "appIcons/" + this.type + ".png";
             appIcon.alt = "Icon";
+            appIcon.ondragstart = function() { return false; };
             appIcon.classList.add("appIcon");
             header.appendChild(appIcon);
 
@@ -58,6 +73,9 @@ Application.prototype.createHTML = function(){
             closeWindow.alt = "Close window";
             closeWindow.src = this.imageFolder + "closewindow.png";
             closeWindow.onclick = function(){
+                if(typeof self.app.ClearTimers == "function"){
+                    self.app.ClearTimers();
+                }
                 document.querySelector("#desktopApplication").removeChild(divApp);
             };
             header.appendChild(closeWindow);
@@ -84,6 +102,9 @@ Application.prototype.createHTML = function(){
             minimizewindow.classList.add("appController");
             minimizewindow.alt = "Minimize";
             minimizewindow.src = this.imageFolder + "minimizewindow.png";
+            minimizewindow.onclick = function(){
+                self.desktop.hideApp(this, self.imageFolder + "appIcons/" + self.type + "_Large.png");
+            };
             header.appendChild(minimizewindow);
 
     divApp.appendChild(header);
@@ -92,25 +113,54 @@ Application.prototype.createHTML = function(){
     var main = document.createElement("main");
 
     var application = document.createElement("div");
-
+    //TODO Break out into separate objects
     switch(this.type){
         case "Memory":
-            new MemoryGame(application, 4, 4);
+            this.app = new MemoryGame(application, 4, 4);
             break;
         case "Chat":
-            new MessageBoard(application, "Chatt");
+            this.app = new MessageBoard(application, "Chatt");
             break;
         case "Minesweeper":
-            new Minesweeper(application);
+            this.app = new Minesweeper(application);
             break;
         case "Quiz":
-            new Questioner(application);
+            this.app = new Questioner(application);
             break;
     }
 
     main.appendChild(application);
     divApp.appendChild(main);
+    if(this.resizable) {
+        var startX, startY, startWidth, startHeight;
+        var initDrag = function(e) {
+            startX = e.clientX;
+            startY = e.clientY;
+            startWidth = parseInt(document.defaultView.getComputedStyle(divApp).width, 10);
+            startHeight = parseInt(document.defaultView.getComputedStyle(divApp).height, 10);
+            document.documentElement.addEventListener('mousemove', doDrag, false);
+            document.documentElement.addEventListener('mouseup', stopDrag, false);
+        };
+        var doDrag = function(e) {
+            divApp.style.minWidth = (startWidth + e.clientX - startX) + 'px';
+            divApp.style.minHeight = (startHeight + e.clientY - startY) + 'px';
+        };
+        var stopDrag = function(e) {
+            document.documentElement.removeEventListener('mousemove', doDrag, false);
+            document.documentElement.removeEventListener('mouseup', stopDrag, false);
+        };
 
+        var resize = document.createElement("img");
+        resize.alt = "Resize";
+        resize.src = this.imageFolder + "resize.png";
+        resize.className = "resize noselect";
+        resize.ondragstart = function() { return false; };
+        resize.addEventListener('mousedown', initDrag, false);
+
+
+
+        divApp.appendChild(resize);
+    }
     document.querySelector("#desktopApplication").appendChild(divApp);
 
 };
