ede6fdcee52631de7106b683449c2b3b21d6dcde
eh222ve <eh222ve@student.lnu.se>
2014-11-28 18:18:01 +0100

    Testing TODO

--- 5-pwd/js/script.js
+++ 5-pwd/js/script.js
@@ -1,6 +1,5 @@
 "use strict";
 
-var app = new Application("Memory", 10, 50);
-var app2 = new Application("Minesweeper", 500, 500);
-var app2 = new Application("Chat", 200, 100);
-var app2 = new Application("Quiz", 500, 50);
+window.onload = function(){
+    new Desktop("desktopApplication");
+};
