1ad2288a3ba75c7b9ac59545621fd53c19e8ce13
eh222ve <eh222ve@student.lnu.se>
2014-12-12 11:41:29 +0100

    Updated dave-dependencies

--- 5-pwd/gulpfile.js
+++ 5-pwd/gulpfile.js
@@ -1,17 +1,6 @@
 var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    minifycss = require('gulp-minify-css'),
-    jshint = require('gulp-jshint'),
     uglify = require('gulp-uglify'),
-    imagemin = require('gulp-imagemin'),
-    rename = require('gulp-rename'),
-    //concat = require('gulp-concat'),
-    concat = require('gulp-concat-util'),
-    notify = require('gulp-notify'),
-    cache = require('gulp-cache'),
-    livereload = require('gulp-livereload'),
-    del = require('del');
+    concat = require('gulp-concat-util');
 
 
 gulp.task('Concat lib', function() {
